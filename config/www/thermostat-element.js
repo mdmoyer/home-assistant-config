1360d35a76fbffe031e2bb5941e3ffe15d1bfa92
Michael Moyer <mdmoyer@gmail.com>
2019-02-03 17:27:43 -0500

    Added thermostat element for floor plan, and some other random stuff

---
+++ config/www/thermostat-element.js
@@ -0,0 +1,60 @@
+import {
+  LitElement, html
+} from 'https://unpkg.com/@polymer/lit-element@^0.5.2/lit-element.js?module';
+
+class ThermostatElement extends LitElement {
+  static get properties() {
+    return {
+      hass: Object,
+      config: Object,
+    }
+  }
+
+  _render({ hass, config }) {
+    const entityId = config.entity;
+    const hvacState =  hass.states[config.hvac_state].state;
+    const temperatureUnits =  hass.states['sensor.dining_room_thermostat_nest_temperature'].attributes['unit_of_measurement'];
+    const currentTemperature = hass.states[entityId].attributes['current_temperature']
+    const operation_mode = hass.states[entityId].attributes['operation_mode']
+
+    return html`
+      <style>
+        .box {
+          width: 40px;
+          height: 40px;
+          border-radius: 20px;
+          line-height: 40px;
+          text-align: center;
+          color: white;
+        }
+        .temp-value {
+          font-size: 14pt;
+          font-weight: bold;
+        }
+        .temp-units {
+          font-size: 10pt;
+        }
+        .cool {
+          background-color: #0080FF;
+        }
+        .heat {
+          background-color: #F48013;
+        }
+        .off {
+          background-color: black;
+        }
+      </style>
+      <div class$="box ${hvacState}">
+        <span class="temp-value">${currentTemperature}</span>
+      </div>
+    `;
+  }
+
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    this.config = config;
+  }
+}
+customElements.define('thermostat-element', ThermostatElement);
