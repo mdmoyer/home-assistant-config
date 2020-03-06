f7fc547a9399ee3d1cc0ad0cd7d0f14fa512bdb8
Michael Moyer <mdmoyer@gmail.com>
2020-03-06 17:37:04 -0500

    Added input_select for temp_regulation Fixed errors in temp_control and house_mode apps

--- config/appdaemon/apps/house_mode.py
+++ config/appdaemon/apps/house_mode.py
@@ -10,7 +10,7 @@ class HouseMode(hass.Hass):
         self.listen_state(self.away_mode_deactivated_callback, 'input_select.house_mode', old = "Away")
         self.listen_state(self.sleep_mode_activated_callback, 'input_select.house_mode', new = "Asleep")
         self.listen_state(self.sleep_mode_deactivated_callback, 'input_select.house_mode', old = "Asleep")
-        self.run_daily(self.deactivate_sleep_mode_callback, "07:00:00", old = "Asleep")
+        self.run_daily(self.deactivate_sleep_mode_callback, "07:00:00")
 
     def log_house_mode_callback(self, entity, attribute, old, new, kwargs):
         self.log("House mode: " + old + " --> " + new)
@@ -28,10 +28,12 @@ class HouseMode(hass.Hass):
         self.call_service("light/turn_off", entity_id = "all")
 
     def sleep_mode_deactivated_callback(self, entity, attribute, old, new, kwargs):
-        self.log("I should now end temp regulation...but I don't.")
-        temp_control = self.get_app('TempControl')
-        temp_control.end_regulate_temp("climate.dining_room_nest")
+        # Return temperature regulation to normal.
+        self.select_option("input_select.temp_reg", "Normal")
 
-    def deactivate_sleep_mode_callback(self, entity, attribute, old, new, kwargs):
-        self.log("Dectivating sleep mode")
-        self.select_option("input_select.house_mode", "Home")
+    def deactivate_sleep_mode_callback(self, kwargs):
+        # Change house_mode to Home only if it is currently set to Asleep
+        house_mode = input_select.house_mode;
+        if house_mode == 'Asleep':
+            self.log("Dectivating sleep mode")
+            self.select_option("input_select.house_mode", "Home")
--- config/appdaemon/apps/temp_control.py
+++ config/appdaemon/apps/temp_control.py
@@ -10,29 +10,33 @@ class TempControl(hass.Hass):
     HVAC_ACTION_IDLE = "idle"
 
     def initialize(self):
-        # Regulate temperature when sleep mode is activated, everytime the
-        # sensor reports a temperature change, everytime the HVAC turns itself
-        # on or off, or the target temperature is changed.
-        self.listen_state(self.regulate_temp_callback, "input_select.house_mode", new = "Asleep")
-        self.listen_state(self.regulate_temp_callback, self.args["sensor_temp"])
-        self.listen_state(self.regulate_temp_callback, climate_entity, attribute = "hvac_action")
-        self.listen_state(self.regulate_temp_callback, climate_entity, attribute = "temperature")
+        # Start temperature regulation.
+        self.listen_state(self.regulate_temp_callback, "input_select.temp_reg", new = "Nighttime")
+        # Maintain temperature regulation.
+        self.listen_state(self.regulate_temp_callback, self.args["temp_sensor"], constrain_input_select = "input_select.temp_reg,Nighttime")
+        self.listen_state(self.regulate_temp_callback, self.args["climate_entity"], attribute = "hvac_action", constrain_input_select = "input_select.temp_reg,Nighttime")
+        self.listen_state(self.regulate_temp_callback, self.args["climate_entity"], attribute = "temperature", constrain_input_select = "input_select.temp_reg,Nighttime")
+        # End temperature regulation.
+        self.listen_state(self.end_regulate_temp_callback, "input_select.temp_reg", new = "Normal")
+
+    def end_regulate_temp_callback(self, entity, attribute, old, new, kwargs):
+        self.end_regulate_temp(self.args["climate_entity"])
 
     def regulate_temp_callback(self, entity, attribute, old, new, kwargs):
-        self.log("Regulate temperature")
         climate_entity = self.args["climate_entity"]
-        temp_sensor = float(self.get_state(self.args["sensor_temp"]))
+        sensor_temp = float(self.get_state(self.args["temp_sensor"]))
         min_temp = float(self.args["min_temp"])
         max_temp = float(self.args["max_temp"])
-        self.regulate_temp(climate_entity, temp_sensor, min_temp, max_temp)
+        self.regulate_temp(climate_entity, sensor_temp, min_temp, max_temp)
 
     def end_regulate_temp(self, climate_entity):
         self.log("End temperature regulation")
         hvac_mode = self.get_state(climate_entity)
         reset_temp = 75 if hvac_mode == self.HVAC_MODE_COOL else 65 if hvac_mode == self.HVAC_MODE_HEAT else 70
-        tempcon_util.set_climate(hvac_mode, reset_temp)
+        self.set_climate(climate_entity, hvac_mode, reset_temp)
 
-    def regulate_temp(self, climate_entity, temp_sensor, min_temp, max_temp):
+    def regulate_temp(self, climate_entity, sensor_temp, min_temp, max_temp):
+        self.log("Regulate temperature")
         climate_curr_temp = float(self.get_state(climate_entity, attribute = "current_temperature"))
         hvac_mode = self.get_state(climate_entity)
         # Indicates whether the HVAC is actively "heating" or "cooling". If neither, it is "idle".
@@ -42,20 +46,19 @@ class TempControl(hass.Hass):
         if hvac_action == self.HVAC_ACTION_IDLE:
             # If temperature is too high, begin cooling.
             if sensor_temp > max_temp:
-                self.set_climate(self.HVAC_MODE_COOL, climate_curr_temp - 2)
+                self.set_climate(climate_entity, self.HVAC_MODE_COOL, climate_curr_temp - 2)
             # If temperature is too low, begin heating.
             elif sensor_temp < min_temp:
-                self.set_climate(self.HVAC_MODE_HEAT, climate_curr_temp + 2)
+                self.set_climate(climate_entity, self.HVAC_MODE_HEAT, climate_curr_temp + 2)
         # If the HVAC is running but the temperature is comfortable, stop the HVAC.
         elif sensor_temp > min_temp + 1 and sensor_temp < max_temp - 1:
-            self.set_climate(hvac_mode, climate_curr_temp + 1 if hvac_mode == self.HVAC_MODE_COOL else climate_curr_temp - 1 if hvac_mode == self.HVAC_MODE_HEAT else climate_curr_temp)
+            self.set_climate(climate_entity, hvac_mode, climate_curr_temp + 1 if hvac_mode == self.HVAC_MODE_COOL else climate_curr_temp - 1 if hvac_mode == self.HVAC_MODE_HEAT else climate_curr_temp)
 
-    def set_climate(self, hvac_mode, new_target):
-        climate_entity = self.args["climate_entity"]
-        sensor_temp = float(self.get_state(self.args["sensor_temp"]))
+    def set_climate(self, climate_entity, new_hvac_mode, new_target):
+        sensor_temp = float(self.get_state(self.args["temp_sensor"]))
         climate_temp = self.get_state(climate_entity, attribute = "current_temperature")
         old_target = self.get_state(climate_entity, attribute = "temperature")
         hvac_action = self.get_state(climate_entity, attribute = "hvac_action")
-        self.log("Sensor: " + str(sensor_temp) + ", Climate: " + str(climate_temp) + " (" + str(hvac_action) + "), Target: " + str(old_target) + " -> " + str(new_target) + " (" + str(hvac_mode) + ")")
-        self.call_service('climate/set_hvac_mode', entity_id = climate_entity, hvac_mode = hvac_mode)
+        self.log("Sensor: " + str(sensor_temp) + ", Climate: " + str(climate_temp) + " (" + str(hvac_action) + "), Target: " + str(old_target) + " -> " + str(new_target) + " (" + str(new_hvac_mode) + ")")
+        self.call_service('climate/set_hvac_mode', entity_id = climate_entity, hvac_mode = new_hvac_mode)
         self.call_service('climate/set_temperature', entity_id = climate_entity, temperature = new_target)
